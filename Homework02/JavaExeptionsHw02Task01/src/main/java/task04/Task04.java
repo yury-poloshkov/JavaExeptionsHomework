04975f519852594fdb5f780e3900405ddd85ef5f
yury-poloshkov <1286600@google.com>
2023-07-26 02:27:20 +0300

    hw02 task0 completed

--- Homework02/JavaExeptionsHw02Task01/src/main/java/task04/Task04.java
+++ Homework02/JavaExeptionsHw02Task01/src/main/java/task04/Task04.java
@@ -1,4 +1,39 @@
 package task04;
 
+import java.util.Scanner;
+
+/*
+Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+ */
 public class Task04 {
+    public static void main(String[] args) {
+        String input = null;
+        do{
+            try {
+                input = prompt("Введите строку: ");
+            } catch (RuntimeException e) {
+                System.out.println(e);
+                System.out.println("Do it again!");
+                timeOut();
+            }
+        } while (input == null);
+        System.out.println("Application stopped");
+    }
+
+    private static String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        String input = in.nextLine();
+        if (input.isEmpty()){
+            throw new RuntimeException("Illegal data! Null strings forbidden!");
+        }
+        return input;
+    }
+
+    private static void timeOut() {
+        System.out.println("Press ENTER to continue");
+        Scanner stopScn = new Scanner(System.in);
+        stopScn.nextLine();
+    }
 }
