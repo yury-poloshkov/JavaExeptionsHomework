4ef86e100335ec1463614340cafaae989f30f858
yury-poloshkov <1286600@google.com>
2023-07-26 02:28:13 +0300

    hw02 task04 completed

--- Homework02/JavaExeptionsHw02Task01/src/main/java/task04/Task04.java
+++ Homework02/JavaExeptionsHw02Task01/src/main/java/task04/Task04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /*
 Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
 Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
- */
+ */ 
 public class Task04 {
     public static void main(String[] args) {
         String input = null;
